5ee6b29f90a22b87945ef39b5f1081792a7bd3a1
Zacooky <54689262+Zacooky@users.noreply.github.com>
2019-11-05 21:42:31 -0500

    Blocks Class
    Parent class: Blocks
    Subclass: reflect block, opaque block, refract block

---
+++ Blocks.py
@@ -0,0 +1,19 @@
+class Blocks:
+	def __init__(self):
+		self.x = x
+		self.y = y
+
+	def move(self):
+		pass
+
+
+class Reflect_Block(Blocks):
+	pass
+
+
+class Opaque_Block(Blocks):
+	pass
+
+
+class Refract_Block(Blocks):
+	pass
