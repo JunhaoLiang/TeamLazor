df173ee12c7156da565e19a59912850c1d96bf5e
etainwuyiyi <54913969+etainwuyiyi@users.noreply.github.com>
2019-11-06 16:35:36 -0500

    sketch managing the lasers

---
+++ test.py
@@ -0,0 +1,67 @@
+A = (1, 1)
+
+LAZOR_DIRECTION = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+
+
+
+
+class block():
+    def __init__(self, position, fixed=False):
+        self.position = position
+    def edges(self, position):
+        edge_list
+        return
+
+
+class reflect(block):
+    def __init__(self, position):
+        block.__init__(self, self. position)
+
+    def 
+
+
+
+
+class refract(block):
+    pass
+
+class opaque(block):
+    pass
+
+
+class lazor():
+    def __init__(self, start_point, direction):
+        self.start_point = start_point
+        self.direction = direction
+
+    def foward(self, beam_point):
+
+
+    def reflect():
+        change_direction(current)
+
+    def absorbed():
+
+    def keep():
+
+
+
+
+
+if __name__ == '__main__':
+    block1 = block(1, 1)
+    block2 = block(2, 2)
+
+    edge_list = []
+    edge_list.apend()
+    lazor_list = []
+    lazor_list.append(lazor_0)
+    lazor_0.
+
+    while(point not in edge_list):
+        lazor_0.forward()
+
+
+
+
